51165e907c3af58f3650d5fb6422b7af289ad4f2
alden.hart <alden08@harts.org>
2015-04-24 05:15:31 -0400

    interim commit

--- TinyG2/tinyg2.h
+++ TinyG2/tinyg2.h
@@ -38,7 +38,7 @@
 /****** REVISIONS ******/
 
 #ifndef TINYG_FIRMWARE_BUILD
-#define TINYG_FIRMWARE_BUILD   		083.24 // arc testing
+#define TINYG_FIRMWARE_BUILD   		083.25 // arc testing
 #endif
 
 #define TINYG_FIRMWARE_VERSION		0.98						// firmware major version
